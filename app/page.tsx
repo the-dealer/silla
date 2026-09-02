2d1e3105b823315ef72c349db2bef69b5032d918
the-dealer <info@thehealthdealer.com>
2026-09-02 10:37:22 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,1 @@
-import Hero from "@/components/Hero"
-import Mecanismo from "@/components/Mecanismo"
-import Protocolo from "@/components/Protocolo"
-import Score from "@/components/Score"
 
-export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <Mecanismo />
-      <Protocolo />
-      <Score />
-    </main>
-  )
-}
